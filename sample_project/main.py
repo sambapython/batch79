29aa17a5b66a63b849731a35fd90a8e5e22aa0e0
sambasivarao.guduru <sambasivarao.guduru@ptw.com>
2021-10-13 09:21:42 +0530

    10/13 assignments.

---
+++ sample_project/main.py
@@ -0,0 +1,11 @@
+print("Product menu:\n\t1.Add product\n\t2.updateproduct\n\t3.search for product")
+option = input("enter an option:")
+if option =="1":
+	p_id = input("Enter product id:")
+	if True:
+		print("it duplicated")
+		p_id = input("enter product id:(Q for quit)")
+		if p_id == "Q":
+			#break
+			pass
+	name = input("Enter product name:")
