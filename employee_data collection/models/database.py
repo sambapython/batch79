8e6f7b7e3a8472f9f8903cbbb571dc751c53b77f
sambasivarao.guduru <sambasivarao.guduru@ptw.com>
2021-11-01 09:38:59 +0530

    11/1oops continues

--- employee_data collection/models/database.py
+++ employee_data collection/models/database.py
@@ -1,14 +1,1 @@
-import psycopg2
-import constants
-def get_conn():
-    dns = f"dbname={constants.DB_NAME} user={constants.USER} password={constants.PASSWORD}"
-    con = psycopg2.connect(dns)
-    cur = con.cursor()
-    return con, cur
-
-def execute_query(query):
-    con,cur = get_conn()
-    cur.execute(query)
-    con.commit()
-    con.close()
 
